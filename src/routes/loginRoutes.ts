4b6cc3d6b06fbcc656d4956b45ec4b8b25afd020
Chr15t3 <gustavopchriste@gmail.com>
2025-03-24 15:22:45 -0300

    Add logout route and req session

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -40,4 +40,32 @@ router.post('/login', (req: RequestWithBody, res)=>{
 
 });
 
+router.get("/", (req, res) => {
+if (req.session && req.session.loggedIn) {
+    res.send(`
+        
+        <div>
+        
+        <div> You are logged in</div>
+        <a href="/logout"> Logout</a>
+
+        </div>
+        
+        `);
+} else {
+    res.send(`
+        
+        <div>
+        
+        <div> You are not logged in </div>
+        <a href="/login">Login</a>
+
+
+        </div>
+        
+        `)
+}
+
+})
+
 export { router };
