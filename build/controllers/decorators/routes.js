c003e350367d0083c364cc2b08c322e7db454482
Chr15t3 <gustavopchriste@gmail.com>
2025-04-21 18:02:03 -0300

    Refactor routes.ts

--- build/controllers/decorators/routes.js
+++ build/controllers/decorators/routes.js
@@ -1,10 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.get = get;
+exports.patch = exports.del = exports.post = exports.put = exports.get = void 0;
 require("reflect-metadata");
 // Decorador para métodos GET
-function get(path) {
-    return function (target, key, descriptor) {
-        Reflect.defineMetadata('path', path, target, key); // Isso está correto.
+function routeBinder(method) {
+    return function get(path) {
+        return function (target, key, desc) {
+            Reflect.defineMetadata('path', path, target, key); // Isso está correto.
+            Reflect.defineMetadata('method', method, target, key);
+        };
     };
 }
+exports.get = routeBinder('get');
+exports.put = routeBinder('put');
+exports.post = routeBinder('post');
+exports.del = routeBinder('get');
+exports.patch = routeBinder('patch');
--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -2,8 +2,18 @@
 import 'reflect-metadata'
 
 // Decorador para métodos GET
-export function get(path: string) {
-  return function (target: any, key: string, descriptor: PropertyDescriptor) {
+function routeBinder(method: string){ //Retorna um Decorator Fábrica
+ return function get(path: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor) {
     Reflect.defineMetadata('path', path, target, key) // Isso está correto.
+    Reflect.defineMetadata('method', method, target, key)
+    }
   }
 }
+
+
+export const get = routeBinder('get')
+export const put = routeBinder('put')
+export const post = routeBinder('post')
+export const del = routeBinder('get')
+export const patch = routeBinder('patch')
